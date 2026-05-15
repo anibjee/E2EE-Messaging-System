511d23ee7850fb685980fe969722c038d83439c6
Aniruddha Bhattacharjee <anibjee@gmail.com>
2026-05-15 19:55:57 +0530

    Configure PostgreSQL in application.properties and update backend CI to use Maven Wrapper

---
+++ backend-java/src/main/java/com/anibjee/e2eebackend/E2eeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.anibjee.e2eebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class E2eeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(E2eeBackendApplication.class, args);
+	}
+
+}
---
+++ backend-java/src/test/java/com/anibjee/e2eebackend/E2eeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anibjee.e2eebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class E2eeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
